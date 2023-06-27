51051d4a69953f80384c8e6c89fb54735d219ebe
Leon Chan <leon.chan@thisisgusto.com>
2023-06-27 18:03:05 +0800

    Day end commit

--- PiggyBank/Controllers/TitleViewController.swift
+++ PiggyBank/Controllers/TitleViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TitleViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
---
+++ PiggyBank/Views/PiggyCell.swift
@@ -0,0 +1,19 @@
+//
+//  PiggyCell.swift
+//  PiggyBank
+//
+//  Created by Leon Chan on 27/6/2023.
+//
+
+import UIKit
+
+class PiggyCell: UICollectionViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
